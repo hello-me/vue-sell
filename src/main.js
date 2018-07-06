c7b68416ddd6b154918d45bd673378855643e459
hello-me <hello_world321@sina.com>
2018-07-06 15:27:00 +0800

    ···

--- src/main.js
+++ src/main.js
@@ -1,19 +1,46 @@
 import Vue from 'vue'
 import App from './App'
 import IndexPage from './pages/index'
+import DetailPage from './pages/detail'
+import DetailAnaPage from './pages/detail/analysis'
+import DetailCouPage from './pages/detail/count'
+import DetailForPage from './pages/detail/forecast'
+import DetailPubPage from './pages/detail/publish'
 import VueRouter from 'vue-router'
-import VueResource from 'vue-resource'
 Vue.config.productionTip = false;
 Vue.use(VueRouter)
-Vue.use(VueResource)
 let router = new VueRouter({
+  mode: 'history',
   routes: [
     {
-    path: '/',
-    component: IndexPage
+      path: '/',
+      component: IndexPage
+    },
+    {
+      path: '/detail',
+      component: DetailPage,
+      redirect: '/detail/analysis',
+      children: [
+        {
+          path: 'analysis',
+          component: DetailAnaPage
+        },
+        {
+          path: 'count',
+          component: DetailCouPage
+        },
+        {
+          path: 'forecast',
+          component: DetailForPage
+        },
+        {
+          path: 'publish',
+          component: DetailPubPage
+        }
+      ]
     }
- ]
- })
+  ]
+})
 new Vue({
   el: '#app',
   router,
